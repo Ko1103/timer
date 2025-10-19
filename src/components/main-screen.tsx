90a28468862365e5ce78538442543c88960d5c24
山浦功 <ko.foot1103@gmail.com>
2025-10-19 21:24:33 +0900

    feat: update main ui

--- src/components/main-screen.tsx
+++ src/components/main-screen.tsx
@@ -4,10 +4,12 @@ import { CountdownTimer } from '@/components/countdown-timer';
 import { Button } from '@/components/ui/button';
 import { useTimer } from '@/hooks/use-timer';
 import { PlayIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 import { TimerLayout } from './layout';
 import { TimerFooter } from './timer-footer';
 
 export function MainScreen() {
+  const { t } = useTranslation();
   const timer = useTimer();
   const first = timer.timers[0] ?? null;
 
@@ -25,100 +27,116 @@ export function MainScreen() {
 
   return (
     <TimerLayout className="flex flex-col gap-4">
-      <div className="text-2xl font-bold text-muted-foreground mt-8 px-2">
-        気分で選ぶ
+      <div className="text-xl font-bold text-muted-foreground mt-12 px-2 text-center">
+        {t('mainScreen.moodTitle')}
       </div>
       {/* Button Section */}
-      <div className="space-y-4 border-t border-border px-4">
-        <div className="flex flex-col gap-3 ">
-          <Button
-            className="h-14 w-full text-lg bg-emerald-300 justify-left"
-            onClick={() => {
-              timer.setTimer([
-                { mode: 'focus', minutes: 5 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 15 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 30 },
-              ]);
-            }}
-          >
-            やる気ない： 5-15-30分セット
-          </Button>
-          <Button
-            className="h-14 w-full text-lg justify-left bg-emerald-500"
-            onClick={() => {
-              timer.setTimer([
-                { mode: 'focus', minutes: 30 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 30 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 30 },
-              ]);
-            }}
-          >
-            いつも通り： 30-30-30分セット
-          </Button>
-          <Button
-            className="h-14 w-full text-lg justify-left bg-emerald-700"
-            onClick={() => {
-              timer.setTimer([
-                { mode: 'focus', minutes: 30 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 60 },
-                { mode: 'rest', minutes: 10 },
-                { mode: 'focus', minutes: 60 },
-              ]);
-            }}
-          >
-            やる気満々： 30-60-60分セット
-          </Button>
-          <Button
-            className="h-14 w-full text-lg justify-left bg-emerald-900"
-            onClick={() => {
-              timer.setTimer([
-                { mode: 'focus', minutes: 60 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 60 },
-                { mode: 'rest', minutes: 5 },
-                { mode: 'focus', minutes: 60 },
-              ]);
-            }}
-          >
-            禅{'　　　　'}：60-60-60分セット
-          </Button>
-        </div>
+      <div className="grid grid-cols-1 gap-4 mx-auto">
+        <Button
+          className="text-lg bg-emerald-300 justify-left rounded-full text-base h-10"
+          onClick={() => {
+            timer.setTimer([
+              { mode: 'focus', minutes: 5 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 15 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 30 },
+            ]);
+          }}
+        >
+          {t('mainScreen.lazy')}
+        </Button>
+        <Button
+          className="text-lg justify-left bg-emerald-500 rounded-full text-base h-10"
+          onClick={() => {
+            timer.setTimer([
+              { mode: 'focus', minutes: 30 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 30 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 30 },
+            ]);
+          }}
+        >
+          {t('mainScreen.steady')}
+        </Button>
+        <Button
+          className="text-lg justify-left bg-emerald-700 rounded-full text-base h-10"
+          onClick={() => {
+            timer.setTimer([
+              { mode: 'focus', minutes: 30 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 60 },
+              { mode: 'rest', minutes: 10 },
+              { mode: 'focus', minutes: 60 },
+            ]);
+          }}
+        >
+          {t('mainScreen.focus')}
+        </Button>
+        <Button
+          className="text-lg justify-left bg-emerald-900 rounded-full text-base h-10"
+          onClick={() => {
+            timer.setTimer([
+              { mode: 'focus', minutes: 60 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 60 },
+              { mode: 'rest', minutes: 5 },
+              { mode: 'focus', minutes: 60 },
+            ]);
+          }}
+        >
+          {t('mainScreen.zen')}
+        </Button>
       </div>
 
-      <div className="border-t border-border pt-4 flex items-center justify-center gap-4 flex-wrap px-2">
+      <div className="text-xl font-bold text-muted-foreground mt-8 px-2 text-center">
+        {t('mainScreen.timeTitle')}
+      </div>
+      <div className="text-muted-foreground text-base font-semibold text-center">
+        {t('mainScreen.focusTitle')}
+      </div>
+      <div className="grid grid-cols-2 gap-4 w-8/10 mx-auto">
         <Button
-          className="text-muted bg-emerald-300 rounded-full bg"
+          className="text-muted bg-emerald-300 rounded-full text-base"
           onClick={() => {
-            timer.setTimer([{ mode: 'focus', minutes: 15 }]);
+            timer.setTimer([
+              { mode: 'focus', minutes: 15 },
+              { mode: 'rest', minutes: 5 },
+            ]);
           }}
         >
           15:00 <PlayIcon className="size-4" />
         </Button>
         <Button
-          className="text-muted bg-emerald-500 rounded-full"
+          className="text-muted bg-emerald-500 rounded-full text-base"
           onClick={() => {
-            timer.setTimer([{ mode: 'focus', minutes: 30 }]);
+            timer.setTimer([
+              { mode: 'focus', minutes: 30 },
+              { mode: 'rest', minutes: 5 },
+            ]);
           }}
         >
           30:00 <PlayIcon className="size-4" />
         </Button>
         <Button
-          className="text-muted bg-emerald-700 rounded-full"
+          className="text-muted bg-emerald-700 rounded-full text-base"
           onClick={() => {
-            timer.setTimer([{ mode: 'focus', minutes: 60 }]);
+            timer.setTimer([
+              { mode: 'focus', minutes: 60 },
+              { mode: 'rest', minutes: 10 },
+            ]);
           }}
         >
           60:00 <PlayIcon className="size-4" />
         </Button>
         <Button
-          className="text-muted bg-emerald-900 rounded-full"
+          className="text-muted bg-emerald-900 rounded-full text-base"
           onClick={() => {
-            timer.setTimer([{ mode: 'focus', minutes: 90 }]);
+            timer.setTimer([
+              { mode: 'focus', minutes: 90 },
+              { mode: 'rest', minutes: 15 },
+            ]);
           }}
         >
           90:00 <PlayIcon className="size-4" />
@@ -126,7 +144,13 @@ export function MainScreen() {
       </div>
 
       <TimerFooter className="flex justify-between text-muted-foreground">
-        <div>今日の合計: {timer.totalMinutes / 60}時間</div>
+        {/* 何時間何分 */}
+        <div>
+          {t('mainScreen.footer.total', {
+            hours: Math.floor(timer.totalMinutes / 60),
+            minutes: timer.totalMinutes % 60,
+          })}
+        </div>
       </TimerFooter>
     </TimerLayout>
   );
