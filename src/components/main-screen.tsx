f8ca0742351e46e7d71559497e1617cc34d4d630
山浦功 <ko.foot1103@gmail.com>
2025-10-20 14:08:16 +0900

    feat: update info

--- src/components/main-screen.tsx
+++ src/components/main-screen.tsx
@@ -26,121 +26,123 @@ export function MainScreen() {
   }
 
   return (
-    <TimerLayout className="flex flex-col gap-4">
-      <div className="text-xl font-bold text-muted-foreground mt-12 px-2 text-center">
-        {t('mainScreen.moodTitle')}
-      </div>
-      {/* Button Section */}
-      <div className="grid grid-cols-1 gap-4 mx-auto">
-        <Button
-          className="text-lg bg-emerald-300 justify-left rounded-full text-base h-10"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 5 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 15 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 30 },
-            ]);
-          }}
-        >
-          {t('mainScreen.lazy')}
-        </Button>
-        <Button
-          className="text-lg justify-left bg-emerald-500 rounded-full text-base h-10"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 30 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 30 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 30 },
-            ]);
-          }}
-        >
-          {t('mainScreen.steady')}
-        </Button>
-        <Button
-          className="text-lg justify-left bg-emerald-700 rounded-full text-base h-10"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 30 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 60 },
-              { mode: 'rest', minutes: 10 },
-              { mode: 'focus', minutes: 60 },
-            ]);
-          }}
-        >
-          {t('mainScreen.focus')}
-        </Button>
-        <Button
-          className="text-lg justify-left bg-emerald-900 rounded-full text-base h-10"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 60 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 60 },
-              { mode: 'rest', minutes: 5 },
-              { mode: 'focus', minutes: 60 },
-            ]);
-          }}
-        >
-          {t('mainScreen.zen')}
-        </Button>
-      </div>
+    <TimerLayout className="flex flex-col">
+      <div className="flex-1 flex flex-col justify-center gap-6 px-4">
+        <div className="text-xl font-bold text-muted-foreground text-center">
+          {t('mainScreen.moodTitle')}
+        </div>
+        {/* Button Section */}
+        <div className="grid grid-cols-1 gap-4 mx-auto">
+          <Button
+            className="text-lg bg-emerald-300 justify-left rounded-full text-base h-10"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 5 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 15 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 30 },
+              ]);
+            }}
+          >
+            {t('mainScreen.lazy')}
+          </Button>
+          <Button
+            className="text-lg justify-left bg-emerald-500 rounded-full text-base h-10"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 30 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 30 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 30 },
+              ]);
+            }}
+          >
+            {t('mainScreen.steady')}
+          </Button>
+          <Button
+            className="text-lg justify-left bg-emerald-700 rounded-full text-base h-10"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 30 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 60 },
+                { mode: 'rest', minutes: 10 },
+                { mode: 'focus', minutes: 60 },
+              ]);
+            }}
+          >
+            {t('mainScreen.focus')}
+          </Button>
+          <Button
+            className="text-lg justify-left bg-emerald-900 rounded-full text-base h-10"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 60 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 60 },
+                { mode: 'rest', minutes: 5 },
+                { mode: 'focus', minutes: 60 },
+              ]);
+            }}
+          >
+            {t('mainScreen.zen')}
+          </Button>
+        </div>
 
-      <div className="text-xl font-bold text-muted-foreground mt-8 px-2 text-center">
-        {t('mainScreen.timeTitle')}
-      </div>
-      <div className="text-muted-foreground text-base font-semibold text-center">
-        {t('mainScreen.focusTitle')}
-      </div>
-      <div className="grid grid-cols-2 gap-4 w-8/10 mx-auto">
-        <Button
-          className="text-muted bg-emerald-300 rounded-full text-base"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 15 },
-              { mode: 'rest', minutes: 5 },
-            ]);
-          }}
-        >
-          15:00 <PlayIcon className="size-4" />
-        </Button>
-        <Button
-          className="text-muted bg-emerald-500 rounded-full text-base"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 30 },
-              { mode: 'rest', minutes: 5 },
-            ]);
-          }}
-        >
-          30:00 <PlayIcon className="size-4" />
-        </Button>
-        <Button
-          className="text-muted bg-emerald-700 rounded-full text-base"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 60 },
-              { mode: 'rest', minutes: 10 },
-            ]);
-          }}
-        >
-          60:00 <PlayIcon className="size-4" />
-        </Button>
-        <Button
-          className="text-muted bg-emerald-900 rounded-full text-base"
-          onClick={() => {
-            timer.setTimer([
-              { mode: 'focus', minutes: 90 },
-              { mode: 'rest', minutes: 15 },
-            ]);
-          }}
-        >
-          90:00 <PlayIcon className="size-4" />
-        </Button>
+        <div className="text-xl font-bold text-muted-foreground text-center">
+          {t('mainScreen.timeTitle')}
+        </div>
+        <div className="text-muted-foreground text-base font-semibold text-center">
+          {t('mainScreen.focusTitle')}
+        </div>
+        <div className="grid grid-cols-2 gap-4 w-8/10 mx-auto">
+          <Button
+            className="text-muted bg-emerald-300 rounded-full text-base"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 15 },
+                { mode: 'rest', minutes: 5 },
+              ]);
+            }}
+          >
+            15:00 <PlayIcon className="size-4" />
+          </Button>
+          <Button
+            className="text-muted bg-emerald-500 rounded-full text-base"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 30 },
+                { mode: 'rest', minutes: 5 },
+              ]);
+            }}
+          >
+            30:00 <PlayIcon className="size-4" />
+          </Button>
+          <Button
+            className="text-muted bg-emerald-700 rounded-full text-base"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 60 },
+                { mode: 'rest', minutes: 10 },
+              ]);
+            }}
+          >
+            60:00 <PlayIcon className="size-4" />
+          </Button>
+          <Button
+            className="text-muted bg-emerald-900 rounded-full text-base"
+            onClick={() => {
+              timer.setTimer([
+                { mode: 'focus', minutes: 90 },
+                { mode: 'rest', minutes: 15 },
+              ]);
+            }}
+          >
+            90:00 <PlayIcon className="size-4" />
+          </Button>
+        </div>
       </div>
 
       <TimerFooter className="flex justify-between text-muted-foreground">
--- src/main/menu.ts
+++ src/main/menu.ts
@@ -84,73 +84,6 @@ export default class MenuBuilder {
         },
       ],
     };
-    const subMenuEdit: DarwinMenuItemConstructorOptions = {
-      label: 'Edit',
-      submenu: [
-        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
-        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
-        { type: 'separator' },
-        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
-        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
-        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
-        {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          selector: 'selectAll:',
-        },
-      ],
-    };
-    const subMenuViewDev: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'Command+R',
-          click: () => {
-            this.mainWindow.webContents.reload();
-          },
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: 'Alt+Command+I',
-          click: () => {
-            this.mainWindow.webContents.toggleDevTools();
-          },
-        },
-      ],
-    };
-    const subMenuViewProd: MenuItemConstructorOptions = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen',
-          accelerator: 'Ctrl+Command+F',
-          click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          },
-        },
-      ],
-    };
-    const subMenuWindow: DarwinMenuItemConstructorOptions = {
-      label: 'Window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'Command+M',
-          selector: 'performMiniaturize:',
-        },
-        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
-        { type: 'separator' },
-        { label: 'Bring All to Front', selector: 'arrangeInFront:' },
-      ],
-    };
     const subMenuHelp: MenuItemConstructorOptions = {
       label: 'Help',
       submenu: [
@@ -183,13 +116,7 @@ export default class MenuBuilder {
       ],
     };
 
-    const subMenuView =
-      process.env.NODE_ENV === 'development' ||
-      process.env.DEBUG_PROD === 'true'
-        ? subMenuViewDev
-        : subMenuViewProd;
-
-    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
+    return [subMenuAbout, subMenuHelp];
   }
 
   buildDefaultTemplate() {
